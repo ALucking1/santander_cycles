7f693c8bcac21233f4b0530c0016bac60d8fe5b8
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-04 14:14:45 +0000

    all tests passing

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -4,13 +4,18 @@ class DockingStation
 
   attr_reader :bike
 
+  # def initialize(capacity=1)
+  #   @capacity = capacity
+  # end
+
+  def dock(bike)
+    fail 'Docking station is at capacity' if @bike
+    @bike = bike
+  end
+
   def release_bike
     fail 'No bikes available' unless @bike
     @bike
-  end
-
-  def dock(bike)
-    @bike = bike
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,21 +3,26 @@ require 'bike'
 
 describe DockingStation do
 
+  # it 'has a maximum capacity of 5' do
+  #   expect(subject.capacity).to eq(1)
+  # end
   describe '#release_bike' do
+    it 'should not release a bike when none are available' do
+      expect{subject.release_bike}.to raise_error 'No bikes available'
+    end
+
     it 'releases a bike' do
       bike = Bike.new
       subject.dock(bike)
-      expect(subject.dock(bike)).to eq(bike)
+      expect(subject.release_bike).to eq(bike)
     end
 
     it 'releases working bikes' do
+      ds = DockingStation.new
       bike = Bike.new
-      subject.release_bike
+      ds.dock(bike)
+      ds.release_bike
       expect(bike).to be_working
-    end
-
-    it 'should not release a bike when none are available' do
-      expect { subject.release_bike }.to raise_error 'No bikes available'
     end
   end
 
@@ -26,6 +31,11 @@ describe DockingStation do
       bike = Bike.new
       subject.dock(bike)
       expect(subject.bike).to eq(bike)
+    end
+
+    it 'should not dock a bike if at capacity' do
+      subject.dock(Bike.new)
+      expect{subject.dock(Bike.new)}.to raise_error 'Docking station is at capacity'
     end
   end
 
