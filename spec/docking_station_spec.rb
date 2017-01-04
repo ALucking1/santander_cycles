93c26e4947841710512e69123308284da526f0b3
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-04 15:15:57 +0000

    add default capacity constant

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -31,7 +31,7 @@ describe DockingStation do
     end
 
     it 'should not dock a bike if at capacity' do
-      20.times {subject.dock(Bike.new)}
+      DockingStation::DEFAULT_CAPACITY.times {subject.dock(Bike.new)}
       expect{subject.dock(Bike.new)}.to raise_error 'Docking station is at capacity'
     end
   end
