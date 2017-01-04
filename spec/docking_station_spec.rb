04d4c2487759008f6632238d1ebdf15c0e47d8e3
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-04 12:39:47 +0000

    raise error test failing

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,31 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  it { is_expected.to respond_to :release_bike }
 
-  it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.dock(bike)).to eq(bike)
+    end
+
+    it 'releases working bikes' do
+      bike = Bike.new
+      subject.release_bike
+      expect(bike).to be_working
+    end
+
+    it 'should not release a bike when none are available' do
+      expect { subject.release_bike }.to raise_error 'No bikes available'
+    end
+  end
+
+  describe '#dock(bike)' do
+    it 'should dock a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.bike).to eq(bike)
+    end
   end
 
 end
