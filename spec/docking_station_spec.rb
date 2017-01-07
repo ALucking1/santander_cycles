e818678b5ba11c8b636b85d20a55d37ec8396903
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-07 13:54:42 +0000

    should not release broken bike - error not being called. only test that's failing.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -17,11 +17,6 @@ let(:bike) {double :bike}
       expect{subject.release_bike}.to raise_error 'No bikes available'
     end
 
-    it 'releases a bike' do
-      subject.dock(bike)
-      expect(subject.release_bike).to eq(bike)
-    end
-
     it 'releases working bikes' do
       allow(bike).to receive(:broken).and_return(false)
       subject.dock(bike)
@@ -42,6 +37,8 @@ let(:bike) {double :bike}
     end
 
     it 'should not release a bike if the bike is broken' do
+      allow(bike).to receive(:broken).and_return(false)
+      allow(bike).to receive(:report_broken).and_return(true)
       bike.report_broken
       subject.dock(bike)
       expect{subject.release_bike}.to raise_error 'No bikes available'
