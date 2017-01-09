3d087028aa8011d0ae84cd89cd5871dfd7d60929
Anna Lucking <annavlucking@hotmail.co.uk>
2017-01-09 11:58:17 +0000

    add test for collecting broken bikes

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,6 +1,12 @@
 require 'van'
+require 'docking_station'
+require 'bike'
 
 describe Van do
+
+  let(:bike) {Bike.new}
+  let(:van) {Van.new}
+  let(:ds) {DockingStation.new}
 
   describe 'capacity' do
     it 'allows the user to set capacity for the van' do
@@ -9,9 +15,18 @@ describe Van do
     end
 
     it 'sets a default capacity if no capacity is given' do
-      van = Van.new
       expect(van.capacity).to eq(20)
     end
   end
 
+  describe '#collect_broken_bikes' do
+    it 'takes broken bikes from the docking station' do
+      bike.report_broken
+      ds.dock(bike)
+      ds.find_broken_bikes
+      bikes = ds.broken_bikes
+      van.collect_broken_bikes(*bikes)
+      expect(van.collected_bikes).to include(bike)
+    end
+  end
 end
